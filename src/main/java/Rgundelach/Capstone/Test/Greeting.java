ea499e25a1d148bc8ef41301c542d3172cb83641
Rgundelach-Neu <129238641+Rgundelach-Neu@users.noreply.github.com>
2024-10-02 12:30:27 -0600

    Initial commit

---
+++ src/main/java/Rgundelach/Capstone/Test/Greeting.java
@@ -0,0 +1,30 @@
+/**
+ * @author rgundelach
+ * @createdOn 10/1/2024 at 12:53 PM
+ * @projectName Capstone
+ * @packageName Rgundelach.Capstone.Test;
+ */
+package Rgundelach.Capstone.Test;
+//Standard Model use for Users
+public class Greeting {
+
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
